1326b237e2b4c1f4f2708ff1ca7c0758432a7ad1
Mohammad Abdulraheem <mhmd.dagamseh.2007@gmail.com>
2026-06-05 03:32:55 +0300

    Add files via upload

---
+++ Problem Solving Level #2/countEvenNumbers.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include <cstdlib>
+#include <ctime>
+#include "myLibrary.h"
+using namespace std;
+void readArrayElements(int& arrLength, int arr[])
+{
+   for (int i = 0; i < arrLength; i++)
+   {
+      arr[i] = getRandNumber(10, 100);
+    }
+   }
+   
+void countEvenNumbers( int arr[], int arrLength,bool isOdd[],int &Counter)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+       if (isOdd[i] == false) {
+          Counter++;
+         }
+      }
+      
+   }
+   void isOdd(int arr[],int arrLength,bool Answer[]) 
+   {
+      for (int i = 0;i < arrLength ; i++) 
+      {
+         if ((arr[i] % 2) == 1) 
+         Answer[i] = true;
+         else 
+         Answer[i] = false;
+      }
+   }
+   
+int main() {
+    srand((unsigned)time(NULL));
+    int arr[100], arrLength,Counter = 0;
+    bool Answer[100];
+    arrLength = readPositiveNumber("Enter A Number Of Array Length");
+    readArrayElements(arrLength, arr);
+    isOdd(arr,arrLength,Answer); 
+    countEvenNumbers(arr, arrLength,Answer,Counter);
+    cout << endl;
+    cout << "count of even numbers are " << Counter << endl;
+   system("pause");
+}
---
+++ Problem Solving Level #2/countPositiveNumber.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+#include <cstdlib>
+#include <ctime>
+#include "myLibrary.h"
+using namespace std;
+int readNumber(string Message)
+{
+    int Number;
+    cout << Message << " : ";
+    cin >> Number;
+    return Number;
+}
+void readArrayElements(int& arrLength, int arr[])
+{
+   for (int i = 0; i < arrLength; i++)
+   {
+      arr[i] = getRandNumber(-1000, 1000);
+    }
+   }
+   
+void countOddNumbers( int arr[], int arrLength,bool isPossitiveNumber[],int &Counter)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+       if (isPossitiveNumber[i] == true) {
+          Counter++;
+         }
+      }
+      
+   }
+   void isPossitiveNumber(int arr[],int arrLength,bool Answer[]) 
+   {
+      for (int i = 0;i < arrLength ; i++) 
+      {
+         if (arr[i] > 0) 
+         Answer[i] = true;
+         else 
+         Answer[i] = false;
+      }
+   }
+   
+int main() {
+    srand((unsigned)time(NULL));
+    int arr[100], arrLength,Counter = 0;
+    bool Answer[100];
+    arrLength = readNumber("Enter A Number Of Array Length");
+    readArrayElements(arrLength, arr);
+    isPossitiveNumber(arr,arrLength,Answer); 
+    countOddNumbers(arr, arrLength,Answer,Counter);
+    cout << endl;
+    cout << "count of positive numbers are " << Counter << endl;
+   system("pause");
+}
